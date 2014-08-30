889b9ce7b2d143ca9c9da23d49052e0de08389d4
daniel-ltw <Daniel.ltw@gmail.com>
2014-08-30 22:25:41 +1200

    Clean up redundant code and comments. Added option menu. Menu include refresh to reload a new set of the map and markers.

--- app/src/main/java/info/technikality/nzhighway/MapsActivity.java
+++ app/src/main/java/info/technikality/nzhighway/MapsActivity.java
@@ -11,6 +11,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,17 +20,11 @@ import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
@@ -182,27 +178,10 @@ public class MapsActivity extends FragmentActivity {
                 publishProgress((int) (i + 1));
 
                 Elements[] results = dataQuery(i);
-                Elements labels = results[0];
                 Elements values = results[1];
 
-                // remove entry that does not have 3 points to trilaterate
-                /*while ((i + 1) < entryList.size() && (labels.size() < 16
-                        || labels.get(8).childNodeSize() != 0
-                        || labels.get(9).childNodeSize() != 0
-                        *//*|| !value.get(12).text().equalsIgnoreCase("Active")*//*)) {
-                   results = dataQuery(i++);
-                   labels = results[0];
-                   values = results[1];
-                }*/
-
-                // check for last entry
-                /*if (i >= entryList.size() - 1) {
-                    return treis;
-                }*/
-
-                // have to fix this to reduce the need to store redundant data
-                // proceed to the trilateration with this data immediately
-                // need not store
+                if (values.size() <= 3)
+                    continue;
 
                 // 1 is LRMS code
                 // 3 & 2 for title
@@ -212,21 +191,9 @@ public class MapsActivity extends FragmentActivity {
                 // 5 for Detour description
                 // 12 for active
                 // 7,8,9 for the 3 points
-                //ArrayList<Object[]> store = new ArrayList<Object[]>();
 
-                /*for (int j = 7; j <= 9; j++) {
-                    Object[] objects = null;
-                    while (objects == null) {
-                        objects = retrieveCoordinates(values.get(j).text().split(" of "));
-                    }
-                    store.add(objects);
-                }
-
-                LatLng coord = trilaterate(store);*/
-
-                // Comment out trilateration
-                // TODO work on SOAP request on LRMS
-                // LRMS code with a 1 RS
+                // SOAP request on LRMS
+                // LRMS code with a 1 or more RS
                 // 0 = SH code
                 // 1 = RS code
                 // 2 = distance in KM
@@ -253,15 +220,6 @@ public class MapsActivity extends FragmentActivity {
                 }
                 markerList.add(mo);
 
-                    /*for (int j = 0; j < label.size(); j++) {
-                        if (j == 7 || j == 8 || j == 9) {
-                            // Direct Line Distance Parameters
-                            treisEntry.put(label.get(7).text().replace(":", " " + (j - 6) + ":"), value.get(j).text());
-                        } else {
-                            treisEntry.put(label.get(j).text(), value.get(j).text());
-                        }
-                    }*/
-                //treis.add(treisEntry);
             }
             return treis;
         }
@@ -285,7 +243,7 @@ public class MapsActivity extends FragmentActivity {
             try {
                 LQJlrmsPoint lm = new LQJLrmsPortBinding().locatePointAlongRs(values[0],
                         Integer.parseInt(values[1]), Double.parseDouble(values[2]) * 1000,
-                        (values[3] == null || values[3].equals(""))? "B":values[3], 4326);
+                        (values[3] == null || values[3].length() != 1)? "B":values[3], 4326);
                 // lm y = latitude
                 // lm x = longitude
                 return new LatLng(lm.y,lm.x);
@@ -342,276 +300,7 @@ public class MapsActivity extends FragmentActivity {
             }
             return null;
         }
-
-        private LatLng trilaterate (ArrayList<Object[]> store) {
-            // refactored trilateration algorithm based on
-            // http://gis.stackexchange.com/questions/66/trilateration-using-3-latitude-and-longitude-points-and-3-distances
-            int earthR = 6371;
-            double[] eX = new double[3];
-            double[] eY = new double[3];
-            double[] eZ = new double[3];
-
-            double p1x = ((LatLng)store.get(0)[2]).latitude;
-            double p2x = ((LatLng)store.get(1)[2]).latitude;
-            double p3x = ((LatLng)store.get(2)[2]).latitude;
-
-            double p1y = ((LatLng)store.get(0)[2]).longitude;
-            double p2y = ((LatLng)store.get(1)[2]).longitude;
-            double p3y = ((LatLng)store.get(2)[2]).longitude;
-
-            double r1 = ( Double.parseDouble((String) store.get(0)[0]) + 0.50 );
-            double r2 = ( Double.parseDouble((String) store.get(1)[0]) + 0.50 );
-            double r3 = ( Double.parseDouble((String) store.get(2)[0]) + 0.50 );
-
-            double xA = earthR *(Math.cos(Math.toRadians(p1x)) * Math.cos(Math.toRadians(p1y)));
-            double yA = earthR *(Math.cos(Math.toRadians(p1x)) * Math.sin(Math.toRadians(p1y)));
-            double zA = earthR *(Math.sin(Math.toRadians(p1x)));
-
-            double xB = earthR *(Math.cos(Math.toRadians(p2x)) * Math.cos(Math.toRadians(p2y)));
-            double yB = earthR *(Math.cos(Math.toRadians(p2x)) * Math.sin(Math.toRadians(p2y)));
-            double zB = earthR *(Math.sin(Math.toRadians(p2x)));
-
-            double xC = earthR *(Math.cos(Math.toRadians(p3x)) * Math.cos(Math.toRadians(p3y)));
-            double yC = earthR *(Math.cos(Math.toRadians(p3x)) * Math.sin(Math.toRadians(p3y)));
-            double zC = earthR *(Math.sin(Math.toRadians(p3x)));
-
-            // A to B
-            double deltaX = (xB - xA);
-            double deltaY = (yB - yA);
-            double deltaZ = (zB - zA);
-            double abNorm = Math.pow(Math.pow(Math.abs(deltaX), 2) + Math.pow(Math.abs(deltaY), 2)
-                    + Math.pow(Math.abs(deltaZ), 2),0.5);
-            double d = abNorm;
-            eX[0] = deltaX / abNorm;
-            eX[1] = deltaY / abNorm;
-            eX[2] = deltaZ / abNorm;
-
-            // A to C
-            deltaX = (xC - xA);
-            deltaY = (yC - yA);
-            deltaZ = (zC - zA);
-            // dot product of eX with AC
-            double i = (eX[0] * deltaX) + (eX[1] * deltaY) + (eX[2] * deltaZ);
-            double acNorm = Math.pow(Math.pow(Math.abs(deltaX - (eX[0] * i)), 2)
-                    + Math.pow(Math.abs(deltaY - (eX[1] * i)), 2)
-                    + Math.pow(Math.abs(deltaZ - (eX[2] * i)), 2),0.5);
-            eY[0] = (deltaX - (eX[0] * i)) / acNorm;
-            eY[1] = (deltaY - (eX[1] * i)) / acNorm;
-            eY[2] = (deltaZ - (eX[2] * i)) / acNorm;
-
-            eZ[0] = (eX[1] * eY[2]) - (eX[2] * eY[1]);
-            eZ[1] = (eX[2] * eY[0]) - (eX[0] * eY[2]);
-            eZ[2] = (eX[0] * eY[1]) - (eX[1] * eY[0]);
-
-            double j = (eY[0] * deltaX) + (eY[1] * deltaY) + (eY[2] * deltaZ);
-
-            double x = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
-            double y = (((r1 * r1) - (r3 * r3) + (i * i) + (j * j)) / (2 * j)) - ((i / j) * x);
-            double z = Double.parseDouble(sqrtImaginaryNum((r1 * r1) - (x * x) - (y * y)));
-
-            //triPt is an array with ECEF x,y,z of trilateration point
-            double[] triPt = new double[3];
-            triPt[0] = xA + x*eX[0] + y*eY[0] + z*eZ[0];
-            triPt[1] = yA + x*eX[1] + y*eY[1] + z*eZ[1];
-            triPt[2] = zA + x*eX[2] + y*eY[2] + z*eZ[2];
-
-            //convert back to lat/long from ECEF
-            //convert to degrees
-            double lat = Math.toDegrees(Math.asin(triPt[2] / earthR));
-            double lon = Math.toDegrees(Math.atan2(triPt[1],triPt[0]));
-
-            Location loc = new Location("");
-            loc.setLatitude(lat);
-            loc.setLongitude(lon);
-
-            return new LatLng(loc.getLatitude(), loc.getLongitude());
-        }
-
-        private String sqrtImaginaryNum(double i) {
-            String str = "";
-            double sqrtI = Math.sqrt(Math.abs(i));
-            if (i < 0) {
-
-                return str += "-" + sqrtI;
-            }
-            return str += sqrtI;
-        }
     }
-
-/*    *//**
-     * Implementation of AsyncTask used to process Marker
-     *
-     * This includes Geocoding the longitude and latitude of the 3 points
-     * and trilateration of those 3 points to give us the exact position
-     * to place the marker on the site.
-     *//*
-    private class ProcessMarkerTask extends AsyncTask<HashMap<String, String>, Void, MarkerOptions> {
-
-        //private ProgressDialog dialog;
-        private Context context;
-        private MarkerOptions markerOptions;
-
-        public ProcessMarkerTask(Context context) {
-            this.context = context;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            *//*dialog = new ProgressDialog(context);
-            dialog.setMessage("Processing Marker ");
-            dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            dialog.setProgress(0);
-            dialog.setMax(100);
-            dialog.show();*//*
-        }
-
-        @Override
-        protected void onPostExecute(MarkerOptions markerOptions) {
-            *//*if (dialog.isShowing()) {
-                dialog.dismiss();
-            }*//*
-
-            int count = 0;
-            while (this.markerOptions == null && count < 5) {
-                try {
-                    Thread.sleep(1000);
-                    count++;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (this.markerOptions != null) {
-
-                if (markerList == null) {
-                    markerList = new ArrayList<MarkerOptions>();
-                }
-
-                markerList.add(this.markerOptions);
-            }
-
-        }
-
-        @Override
-        protected MarkerOptions doInBackground(HashMap<String, String>... hm) {
-            ArrayList<Object[]> store = new ArrayList<Object[]>();
-            for (int i = 1; i < 4; i++) {
-                // object array contain
-                // 1 - distance
-                // 2 - compass direction
-                // 3 - location (longitude,latitude)
-                Object[] objects = new Object[3];
-                try {
-                    objects = retrieveCoordinates(hm[0].get("Direct Line Distances " + i + ":").split(" of "));
-                    store.add(objects);
-                } catch (NullPointerException e) {
-                    Log.e("nzhighway","Direct Line Distance Error: " + (i - 1) + " number of parameter. ");
-                    return null;
-                }
-            }
-
-
-            if (store.size() >= 3 && store.get(0) != null && store.get(1) != null && store.get(2) != null) {
-                // trilateration
-                double[] eX = new double[2]; // unit vector in direction from P1 to P2
-                double[] eY = new double[2]; // unit vector in direction from P1 to P2
-
-                double p1x = ((Location) store.get(0)[2]).getLatitude();
-                double p2x = ((Location) store.get(1)[2]).getLatitude();
-                double p3x = ((Location) store.get(2)[2]).getLatitude();
-
-                double p1y = ((Location) store.get(0)[2]).getLongitude();
-                double p2y = ((Location) store.get(1)[2]).getLongitude();
-                double p3y = ((Location) store.get(2)[2]).getLongitude();
-
-                double r1 = Double.parseDouble((String) store.get(0)[0]) * 1000;
-                double r2 = Double.parseDouble((String) store.get(1)[0]) * 1000;
-                double r3 = Double.parseDouble((String) store.get(2)[0]) * 1000;
-
-	    	*//* Calculating i, the signed magnitude of the x component from P1 to P3 *//*
-                double deltaX = (p2x - p1x);
-                double deltaY = (p2y - p1y);
-                double norm = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                double d = norm;
-
-                eX[0] = deltaX / norm;
-                eX[1] = deltaY / norm;
-
-                deltaX = (p3x - p1x);
-                deltaY = (p3y - p1y);
-                double i = eX[0] * deltaX + eX[1] * deltaY;
-
-    		*//* Calculating j, the signed magnitude of the y component from P1 to P3 *//*
-                deltaX = (p3x - p1x - i * eX[0]);
-                deltaY = (p3y - p1y - i * eX[1]);
-                norm = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-
-                eY[0] = deltaX / norm;
-                eY[1] = deltaY / norm;
-
-                deltaX = (p3x - p1x);
-                deltaY = (p3y - p1y);
-                double j = eY[0] * deltaX + eY[1] * deltaY;
-
-		    *//* Calculate x *//*
-                double x = (r1 * r1 - r2 * r2 + d * d) / (2 * d);
-                double x2 = x;
-
-		    *//* Calculate y *//*
-                double y = (((r1 * r1) - (r3 * r3) + (i * i) + (j * j)) / (2 * j)) - ((i / j) * x);
-
-                x = p1x + x * eX[0] + y * eY[0];
-                y = p1y + x2 * eX[1] + y * eY[1];
-
-                this.markerOptions = new MarkerOptions().position(new LatLng(x, y))
-                        .title(hm[0].get("Event Type:")).snippet(hm[0].get("Location Area:"));
-
-                return this.markerOptions;
-            }
-            return null;
-        }
-
-        private Object[] retrieveCoordinates(String[] coord) {
-            // geocoding + address=santa+cruz&components=country:ES&key=
-            try {
-                URL url = new URL(geocoding + "address=" + coord[1] +
-                        "&components=country:NZ&key=" +
-                        "AIzaSyBH8z5LRQXBw3figE1D3XgnNEwR-52Ed-8");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(20000 *//* milliseconds *//*);
-                conn.setConnectTimeout(30000 *//* milliseconds *//*);
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-                // Starts the query
-                conn.connect();
-                JSONObject jObject = new JSONObject(getStringFromInputStream(conn.getInputStream()));
-                if (jObject.getString("status").equalsIgnoreCase("ok")) {
-                    jObject = jObject.getJSONArray("results").getJSONObject(0);
-                    JSONObject geometry = jObject.getJSONObject("geometry");
-                    JSONObject location = geometry.getJSONObject("location");
-                    Double lng = location.getDouble("lng");
-                    Double lat = location.getDouble("lat");
-                    Location l = new Location("");
-                    l.setLongitude(lng);
-                    l.setLatitude(lat);
-                    String[] vectors = coord[0].split(" km ");
-                    return new Object[]{vectors[0], vectors[1], l};
-                } else {
-                    // Error
-                    return null;
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-    }*/
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
@@ -658,22 +347,6 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void loadMarker() {
-
-        /*while (treis.size() <= entryList.size() - 5) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        // run the entryList from treis and process add the respective marker at the respective coordinates
-        for (HashMap<String, String> hm : treis) {
-            // google geocoding for coordinates
-            // then trilaterate to place markers
-            markerProcess = new ProcessMarkerTask(this).execute(hm);
-        }*/
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         setUpMapIfNeeded();
     }
 
@@ -793,5 +466,18 @@ public class MapsActivity extends FragmentActivity {
 
         return sb.toString();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Refresh Map").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                loadPage();
+                return false;
+            }
+        });
+        menu.add("Navigate");
+        return super.onPrepareOptionsMenu(menu);
     }
 }
